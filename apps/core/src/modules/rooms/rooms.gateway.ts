3f70a962a6e0b10c287b49d12e07a8b88b13369f
AllanOliveiraM <nallamo2016@gmail.com>
2025-05-17 17:26:12 -0300

    feat: set up websockets

--- apps/core/src/modules/rooms/rooms.gateway.ts
+++ apps/core/src/modules/rooms/rooms.gateway.ts
@@ -4,7 +4,11 @@ import { Socket } from 'socket.io'
 
 import { RoomsService } from './rooms.service'
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class RoomsGateway {
   constructor(private readonly roomsService: RoomsService) {
     console.log('entrou')
